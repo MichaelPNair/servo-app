0d4c1e20ff21aeb48a634f5ef6615d7fc2ba78f1
Chatkamon <chatkamon_bee@hotmail.com>
2023-07-18 23:33:13 +1000

    add digital clock design

--- client/components/time.js
+++ client/components/time.js
@@ -5,9 +5,30 @@ function refreshTime() {
   const timeDisplay = document.querySelector(".show-time")
   const date = new Date()
   const dateString = date.toDateString()
-  const timeString = date.toLocaleTimeString()
+  //const timeString = date.toLocaleTimeString()
   
-  timeDisplay.textContent = `${dateString} ${timeString}`
+  timeDisplay.textContent = dateString
 }
 
-setInterval(refreshTime, 1000)
+function updateClock() {
+  const d = new Date(); //object of date()
+  const hr = d.getHours();
+  const min = d.getMinutes();
+  const sec = d.getSeconds();
+  const hr_rotation = 30 * hr + min / 2; //converting current time
+  const min_rotation = 6 * min;
+  const sec_rotation = 6 * sec;
+
+  const hour = document.getElementById("hour");
+  const minute = document.getElementById("minute");
+  const second = document.getElementById("second")
+
+  hour.style.transform = `rotate(${hr_rotation}deg)`;
+  minute.style.transform = `rotate(${min_rotation}deg)`;
+  second.style.transform = `rotate(${sec_rotation}deg)`;
+}
+
+setInterval(() => {
+  refreshTime()
+  updateClock()
+}, 1000)
