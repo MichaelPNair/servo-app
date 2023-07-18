6d94b27f5bddd75e8a0e7ee4645981648cafbd57
Chatkamon <chatkamon_bee@hotmail.com>
2023-07-18 11:42:08 +1000

    add stat info box and petro station

---
+++ client/components/nearest.js
@@ -0,0 +1,32 @@
+import { fetchStations } from "../servos_api.js";
+const nearestSection = document.querySelector(".nearest");
+
+fetchStations()
+  .then(renderStations)
+
+function renderStations(stations) {
+  const firstTenStations = stations.slice(0,10)
+  console.log(firstTenStations)
+  firstTenStations.forEach(station => {
+    const div = document.createElement("div")
+    div.classList.add("stations")
+    const logo = document.createElement("img")
+    logo.classList.add("logo")
+    const contentWrap = document.createElement("div")
+    const topContent = document.createElement("div")
+    topContent.classList.add("nearest-content")
+    const bottomContent = document.createElement("div")
+    bottomContent.classList.add("nearest-content")
+
+    logo.src = station.logo_url
+    topContent.innerHTML = station.owner
+    bottomContent.innerHTML = station.address
+
+    div.appendChild(logo)
+    contentWrap.appendChild(topContent)
+    contentWrap.appendChild(bottomContent)
+    div.appendChild(contentWrap)
+    nearestSection.appendChild(div)
+  })
+}
+
