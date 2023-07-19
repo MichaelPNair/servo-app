30633ee63fbe3af2a66cba0477e54e2d91df3b62
Chatkamon <chatkamon_bee@hotmail.com>
2023-07-19 15:22:46 +1000

    stats info box added

--- client/servos_api.js
+++ client/servos_api.js
@@ -2,3 +2,8 @@ export function fetchStations() {
   return fetch(`/api/stations/all`)
   .then(res => res.json())
 }
+
+export function fetchStats() {
+  return fetch(`/api/stats`)
+  .then(res => res.json())
+}
