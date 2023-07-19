2b030ea1b292c9ca98a4eb8235dc67c9cec43fb3
Chatkamon <chatkamon_bee@hotmail.com>
2023-07-19 14:24:55 +1000

    edit oil price and add get api stats

--- client/components/oil-prices.js
+++ client/components/oil-prices.js
@@ -5,51 +5,23 @@ const date = new Date()
 const dateString = date.toLocaleDateString('en-GB')
 const oilDate = document.querySelector('.date')
 oilDate.textContent = dateString
+const symbols = ['CL', 'BB', 'NG']
+const oilName = ['WTI OIL', 'Brent Oil', 'Natural Gas']
+const oilUnits = ['USD per barrel', 'USD per barrel', 'USD per MMBtu']
+const oilPrice = document.querySelector('.price')
+const prices = ['.wti-price', '.brent-price', '.natural-price']
 
-// const res = await axios.get(
-    //     'api.futures-api.com/symbols', 
-    //       {
-        //         headers: {
-            //             'x-api-key': {CHCzj9LdpT1pTjL76rBC01vhn6ghrZYh82lFaCez}
-            //         }
-            //       }
-            //     )
-            // console.log(res.data)
-
-            const symbols = ['CL', 'BB', 'NG']
-
-            const oilName = ['WTI OIL', 'Brent Oil', 'Natural Gas']
-
-            const oilUnits = ['USD per barrel', 'USD per barrel', 'USD per MMBtu']
-            
-            const oilPrice = document.querySelector('.price')
-            
-            
-            for(let i = 0; i < symbols.length; i++) {
-
-                fetch(`https://api.futures-api.com/last?symbol=${symbols[i]}`, {
-                    headers: {
-                        'x-api-key': 'CHCzj9LdpT1pTjL76rBC01vhn6ghrZYh82lFaCez'
-                    }
-                } )
-                .then(res => res.json())
-                .then(res => {
-                    const last = res.data[0].last
-                    
-              const oil = document.createElement('li')
-              oil.textContent = `${oilName[i]} ${last} ${oilUnits[i]}`
-              oilPrice.appendChild(oil)
-            })
-                
+    for(let i = 0; i < symbols.length; i++) {
+        fetch(`https://api.futures-api.com/last?symbol=${symbols[i]}`, {
+            headers: {
+                'x-api-key': 'CHCzj9LdpT1pTjL76rBC01vhn6ghrZYh82lFaCez'
             }
+        } )
+        .then(res => res.json())
+        .then(res => {
+            const last = res.data[0].last
+            const oil = document.querySelector(prices[i])
+            oil.textContent = `${oilName[i]} ${last} ${oilUnits[i]}`
+        })  
+    }
 
-
-
-
-
-
-          // CL = WTI
-          // NG = NATURAL GAS
-          // BB = BRENT OIL
-
-          // How can we change the parameter without separate fetch requests
