7d2e99e50ee4f62ed880bc042a41e582e34ce263
MichaelPNair <michael.p.nair@gmail.com>
2023-07-19 20:27:34 +1000

    add distances to nearest stations section

--- client/servos_api.js
+++ client/servos_api.js
@@ -7,3 +7,8 @@ export function fetchStats() {
   return fetch(`/api/stats`)
   .then(res => res.json())
 }
+
+export function fetchNearestStations(lat, lng){
+  return fetch(`/api/stations/nearest?lat=${lat}&lng=${lng}&radius=50000`)
+  .then(res => res.json())
+}
