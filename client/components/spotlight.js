a5570afbf508730767cefcd455ea2a6096f73af0
Alessandra Manalansan <sandy.manalansan@gmail.com>
2023-07-20 10:52:53 +1000

    spotlight refresh update

--- client/components/spotlight.js
+++ client/components/spotlight.js
@@ -1,19 +1,33 @@
-fetch('/api/stations/random')
-    .then(response => response.json())
-    .then(data => {
-        const name = document.querySelector('.name')
-        const address = document.querySelector('.address')
-        const logo = document.querySelector('#logo')
-        const logoImage = document.createElement("img")
-        logo.appendChild(logoImage)
-        logoImage.src = data.logo_url
-        logoImage.style.height = '64px'
-        logoImage.style.width = '64px'
-        
-        name.innerText = data.name
-        address.innerText = data.address
-    })
+function refreshSpotlight() {
+    fetch('/api/stations/random')
+        .then(response => response.json())
+        .then(data => {
+            const name = document.querySelector('.name')
+            const address = document.querySelector('.address')
+            const logo = document.querySelector('#logo')
 
-//===== > refresh hyperlink to display another random station
+            logo.innerHTML = ""
+
+            const logoImage = document.createElement("img")
+            logo.appendChild(logoImage)
+            logoImage.src = data.logo_url
+            logoImage.style.height = '64px'
+            logoImage.style.width = '64px'
+        
+            name.innerText = data.name
+            address.innerText = data.address
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error)
+        });
+}
+
+
+refreshSpotlight()
+
 const refresh = document.querySelector('#refresh')
-    refresh.addEventListener('click', fetch('/api/stations/random'))
+
+refresh.addEventListener('click', (event) => {
+    event.preventDefault()
+    refreshSpotlight()
+});
